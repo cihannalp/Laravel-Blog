ec8f3b9a40331883681239e0260fd2bfba687353
cihannalp <cihannalp@gmail.com>
2015-12-24 16:47:50 +0200

    adminpage added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('about','PagesController@about');
+Route::get('contact','PagesController@contact');
+
+Route::get('admin','AdminController@index');
+Route::get('articles','PagesController@index');
